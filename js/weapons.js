99f6843bf70ae751e623699269be744d8d8b9879
Joe Johnson <joe@Joes-MacBook-Pro.local>
2017-12-08 17:03:23 -0800

    In-class code along.

---
+++ js/weapons.js
@@ -0,0 +1,11 @@
+function fireWeapon() {
+  if(game.time.now < weaponTimer){
+    return;
+  }
+  console.log('fire!');
+  var weapon = lasers.getFirstExists(false);
+  weapon.reset(player.x, player.y + WEAPONS[0].offset);
+  weapon.body.velocity.x = WEAPONS[0].velocity;
+  pewpew.play();
+  weaponTimer = game.time.now + WEAPONS[0].timer;
+}
